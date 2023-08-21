fe92a9cdf7ffe421d56eb15ef1f6a8be7d893e22
Peter <yuury.chang@gmail.com>
2023-08-21 13:44:40 +0800

    Implement createContext

--- libs/hooks/create-context/src/index.ts
+++ libs/hooks/create-context/src/index.ts
@@ -1,2 +1,25 @@
-// eslint-disable-next-line @typescript-eslint/no-empty-function
-export const createContext = () => {};
+import {
+  Context,
+  Provider,
+  createContext as createReactContext,
+  useContext as useReactContext,
+} from 'react';
+
+export type CreateContextReturn<T> = [Provider<T>, () => T, Context<T>];
+
+type CreateContextOptions<T> = {
+  defaultValue: T;
+  displayName?: string;
+};
+
+// Refer {@link https://github.com/chakra-ui/chakra-ui/blob/main/packages/hooks/context/src/index.ts}
+export const createContext = <T>(options: CreateContextOptions<T>) => {
+  const { defaultValue, displayName } = options || {};
+
+  const context = createReactContext<T | undefined>(defaultValue);
+  context.displayName = displayName;
+
+  const useContext = () => useReactContext(context);
+
+  return [context.Provider, useContext, context] as CreateContextReturn<T>;
+};
